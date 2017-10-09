3996bb9cdf165dc3bfcce5ca3e8eea11af8597d2
b-sirius <padfoot_07@163.com>
2017-10-09 18:25:59 +0800

    v1.0

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.scss';
+
+var container = document.createElement('div');
+container.innerHTML = '<p class="text">die!!!<p>';
+
+document.body.appendChild(container);
---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path'); // 提供路径处理
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 自动生成html
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 清理 dist 目录
+
+module.exports = {
+    // 文件入口
+    entry: {
+        /* 此处需自己按需设置 */
+        app: './src/index.js'
+    },
+    // 插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'title', // 网站标题
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    module: {
+        // 文件处理
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
